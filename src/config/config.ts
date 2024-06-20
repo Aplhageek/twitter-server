073a22f68d1b55c111f1f99ae8b2e233f36818d5
Jayesh <en001507@gmail.com>
2024-06-21 03:10:47 +0530

    setup config for env data

---
+++ src/config/config.ts
@@ -0,0 +1,43 @@
+// config.ts
+
+const  dotenv = require("dotenv");
+
+
+import Joi from "joi";
+import path from "path";
+
+dotenv.config({ path: path.join(__dirname, "../../backend/.env") });
+
+const envVarSchema = Joi.object().keys({
+    PORT: Joi.number().default(8000),
+    DATABASE_URL: Joi.string().description("Mongo DB Url").required(),
+    NODE_ENV: Joi.string()
+        .valid("production", "development", "test")
+        .required(),
+    AWS_S3_SECRETE_KAY: Joi.string().required().description("S3 Secrete key"),
+    AWS_S3_ACCESS_KEY: Joi.string().required().description("S3 access key"),
+}).unknown();
+
+const { value: envVars, error } = envVarSchema
+    .prefs({ errors: { label: "key" } })
+    .validate(process.env);
+
+if (error) {
+    throw new Error(`Config validation error: ${error.message}`);
+}
+
+const config = {
+    env: envVars.NODE_ENV,
+    port: envVars.PORT,
+    database: {
+        url: envVars.DATABASE_URL,
+    },
+    AWS: {
+        S3: {
+            access_key: envVars.AWS_S3_ACCESS_KEY,
+            secret_key: envVars.AWS_S3_SECRETE_KAY,
+        }
+    }
+};
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { initServer } from "./App"
+import config from "./config/config";
 
-async function init () {
+async function init() {
     const app = await initServer();
-    app.listen(8000, () => console.log(`Server started at PORT : 8000`));
+    app.listen(config.port, () => console.log(`Server started at PORT : 8000`));
 }
+
 
 init();
