f6252fa5daded953df99863b9726697bd0025331
Jayesh <en001507@gmail.com>
2024-06-24 08:08:57 +0530

    Fixed: implimentation of findbyid

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -5,8 +5,8 @@ class UserService {
     public static async findByEmail(email: string) {
        return await prismaClient.user.findUnique({ where: { email } });
     }
-    public static async findById(email: string) {
-       return await prismaClient.user.findUnique({ where: { email } });
+    public static async findById(id: string) {
+       return await prismaClient.user.findUnique({ where: { id } });
     }
     public static async create(user : CreateUser) {
        return await prismaClient.user.create({data : user});
